9ba2e480680d2fa32b4652d9caf954e9532fa526
nitesh-0 <niteshsah760@gmail.com>
2025-02-22 17:31:09 +0545

    changed package.json

--- Backend/dist/db.js
+++ Backend/dist/db.js
@@ -1,11 +1,18 @@
 "use strict";
+// import { Pool } from 'pg';
 Object.defineProperty(exports, "__esModule", { value: true });
+// const pool = new Pool({
+//     user: 'postgres', // Database username
+//     host: 'localhost', // Host (default is localhost)
+//     database: 'firstdb', // Replace with your database name
+//     password: 'restart@123', // Replace with your PostgreSQL password
+//     port: 5432, // Default PostgreSQL port
+// });
+// export default pool;
 const pg_1 = require("pg");
+// Use the DATABASE_URL environment variable if it's set (for production)
 const pool = new pg_1.Pool({
-    user: 'postgres', // Database username
-    host: 'localhost', // Host (default is localhost)
-    database: 'firstdb', // Replace with your database name
-    password: 'restart@123', // Replace with your PostgreSQL password
-    port: 5432, // Default PostgreSQL port
+    connectionString: "postgresql://qsdb_user:Eiw9JW183GXv5b4xVsdeHPteVknuwneK@dpg-cusqvkjqf0us739npn20-a/qsdb", // This will be set in Render's environment variables
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Secure connection for production
 });
 exports.default = pool;
